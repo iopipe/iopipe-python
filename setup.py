ccf337eef22568d2426fc787408a4ef2144e15dc
Eric Windisch <eric@iopipe.com>
2016-08-10 17:57:05 +0000

    Setuptools & requirements.txt
    Signed-off-by: Eric Windisch <eric@iopipe.com>

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from pip.req import parse_requirements
+from pip.download import PipSession
+from setuptools import setup
+
+install_reqs = parse_requirements('./requirements.txt', session=PipSession())
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(name='iopipe',
+      version='0.1',
+      description='IOpipe Collector for serverless Application Performance Monitoring',
+      author='Eric Windisch',
+      author_email='eric@iopipe.com',
+      url='https://github.com/iopipe/iopipe-python',
+      packages=['iopipe'],
+      install_requires=reqs,
+     )
