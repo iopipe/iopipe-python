f5c8b55f68f5bdac128493b8b44cabb29ee3b814
Corey Light <coreylight@gmail.com>
2016-11-28 11:43:34 -0600

    long-running success function for hrtime verification

---
+++ functions/long-running-success/index.js
@@ -0,0 +1,9 @@
+var iopipe = require('iopipe')({ clientId: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImF1dGgwfDU4MTA4ZWU4MDA0NDE0YWU2NzkxMzMyZCIsInVzZXJuYW1lIjoiaW9waXBlX2RlbW8iLCJpYXQiOjE0Nzc0ODAxOTcsImF1ZCI6Imh0dHBzOi8vbWV0cmljcy1hcGkuaW9waXBlLmNvbS9ldmVudC8ifQ.rqy-hDI5x_nSJaQiVUviX5YH6OhzR7HMEQG79d_OuRw" })
+exports.handle = iopipe(
+  function (event, context) {
+    var duration = 2000;
+    setTimeout(function(){
+      context.succeed('Succeeded with a set duration of ' + duration);
+    }, duration);
+  }
+)
