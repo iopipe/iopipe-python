9561311b5c824c6b66e953bdebe75d0dd1ca0919
pselle <pamela.selle@gmail.com>
2016-12-09 15:13:56 -0500

    Try wget

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,64 +1,9 @@
 #!/bin/sh
 
 echo "Installing Apex"
-install () {
-
-set -eu
-
-UNAME=$(uname)
-
-echo $UNAME
-uname -m
-if [ "$UNAME" != "Linux" -a "$UNAME" != "Darwin" -a "$UNAME" != "OpenBSD" ] ; then
-    echo "Sorry, OS not supported: ${UNAME}. Download binary from https://github.com/apex/apex/releases"
-    exit 1
-fi
-
-if [ "$UNAME" = "Darwin" ] ; then
-  OSX_ARCH=$(uname -m)
-  if [ "${OSX_ARCH}" = "x86_64" ] ; then
-    PLATFORM="darwin_amd64"
-  else
-    echo "Sorry, architecture not supported: ${OSX_ARCH}. Download binary from https://github.com/apex/apex/releases"
-    exit 1
-  fi
-elif [ "$UNAME" = "Linux" ] ; then
-  LINUX_ARCH=$(uname -m)
-  if [ "${LINUX_ARCH}" = "i686" ] ; then
-    PLATFORM="linux_386"
-  elif [ "${LINUX_ARCH}" = "x86_64" ] ; then
-    PLATFORM="linux_amd64"
-  else
-    echo "Sorry, architecture not supported: ${LINUX_ARCH}. Download binary from https://github.com/apex/apex/releases"
-    exit 1
-  fi
-elif [ "$UNAME" = "OpenBSD" ] ; then
-    OPENBSD_ARCH=$(uname -m)
-  if [ "${OPENBSD_ARCH}" = "amd64" ] ; then
-      PLATFORM="openbsd_amd64"
-  else
-      echo "Sorry, architecture not supported: ${OPENBSD_ARCH}. Download binary from https://github.com/apex/apex/releases"
-      exit 1
-  fi
-
-fi
-
-LATEST=$(curl -s https://api.github.com/repos/apex/apex/tags | grep -Eo '"name":.*?[^\\]",'  | head -n 1 | sed 's/[," ]//g' | cut -d ':' -f 2)
-URL="https://github.com/apex/apex/releases/download/v0.10.3/apex_linux_amd64"
-DEST=${DEST:-/usr/local/bin/apex}
-
-if [ -z $LATEST ] ; then
-  echo "Error requesting. Download binary from https://github.com/apex/apex/releases"
-  exit 1
-else
-  echo "downloading apex"
-  curl -sL https://github.com/apex/apex/releases/download/v0.10.3/apex_linux_amd64 -o $DEST
-  echo "downloaded"
-  chmod +x $DEST
-fi
-}
-
-install
+wget https://github.com/apex/apex/archive/v0.11.0.tar.gz
+tar xvzf v0.11.0.tar.gz
+cd v0.11.0 && make
 
 for func in $(ls functions); do
   if [ -f functions/${func}/package.json ]; then
