18e45fee06a0221a836594d1b7b55101045096f0
pselle <pamela.selle@gmail.com>
2016-12-27 14:35:23 -0500

    Use our own mock context

--- iopipe/tests/MockContext.py
+++ iopipe/tests/MockContext.py
@@ -1,25 +1,15 @@
-# Inspired by:
-# https://github.com/sportarchive/aws-lambda-python-local/blob/master/tests/MockContext.py
-import random
-
-
 class MockContext(object):
-    def __init__(self, name, version):
+    function_version = '$LATEST'
+    aws_request_id = 0
+    log_group_name = 'mock-group'
+    logs_stream_name = 'mock-stream'
+    memory_limit_in_mb = 500
+
+    def __init__(self, name='handler', version='$LATEST'):
         self.function_name = name
         self.function_version = version
-        self.invoked_function_arn = (
-            """arn:aws:lambda:us-east-1:123456789012:
-            function:{name}:{version}""".format(name=name, version=version))
-        self.memory_limit_in_mb = float('inf')
-        self.log_group_name = 'test-group'
-        self.log_stream_name = 'test-stream'
-        self.client_context = None
-
-        self.aws_request_id = '-'.join(map(
-            lambda n: ''.join(map(
-                lambda _: random.choice('0123456789abcdef'), range(0, n))),
-            [8, 4, 4, 4, 12]
-        ))
+        self.invoked_function_arn = """arn:aws:lambda:us-east-1:1:
+            function:%s:%s""" % (name, version)
 
     def get_remaining_time_in_millis(self):
         return float('inf')
