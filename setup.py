b51f9b977f167ae24320b487a7121379247cf292
pselle <pamela.selle@gmail.com>
2016-12-29 11:47:52 -0500

    Set version in one place, bump version for metrics release

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import re
 
 from pip.req import parse_requirements
 from pip.download import PipSession
@@ -7,8 +8,17 @@ from setuptools import setup
 install_reqs = parse_requirements('./requirements.txt', session=PipSession())
 reqs = [str(ir.req) for ir in install_reqs]
 
+
+with open('iopipe/iopipe.py', 'r') as fd:
+    version = re.search(r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+
 setup(name='iopipe',
-      version='0.1.6',
+      version=version,
       description='IOpipe agent for serverless Application Performance Monitoring',
       author='IOpipe',
       author_email='support@iopipe.com',
