38793b95a7fcec2506b5a45140e368e758292c38
Pam Selle <pamela.selle@gmail.com>
2017-02-02 11:21:31 -0500

    Don't send invalid keys (for now)

--- functions/custom-events/index.js
+++ functions/custom-events/index.js
@@ -3,9 +3,9 @@ exports.handle = iopipe(
   function (event, context) {
     iopipe.log("simple-key", 42);
     iopipe.log("key with space", "a neat value");
-    iopipe.log(124098234, "number as key");
-    iopipe.log(undefined, "undefined key");
-    iopipe.log(null, "null key");
+    // iopipe.log(124098234, "number as key");
+    // iopipe.log(undefined, "undefined key");
+    // iopipe.log(null, "null key");
     iopipe.log('undefined value', undefined);
     iopipe.log('null value', null);
     iopipe.log('long_value', 'Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment. Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.');
