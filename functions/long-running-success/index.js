d194439845fe5477decde7d0cd5eed39329e4f30
Adam Johnson <adjohn@gmail.com>
2017-02-10 14:05:18 -0800

    Adding randomness to qa events (#12)
    * Change duration to .7-2 seconds randomly
    
    To make the data appear more interesting, making this a range.
    
    * Adding a random number in the custom metrics.
    
    In order to make the data more realistic, adding a random value.
    
    * Fixing error with random maths

--- functions/long-running-success/index.js
+++ functions/long-running-success/index.js
@@ -1,7 +1,7 @@
 var iopipe = require('iopipe')({ clientId: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImF1dGgwfDU4MTA4ZWU4MDA0NDE0YWU2NzkxMzMyZCIsInVzZXJuYW1lIjoiaW9waXBlX2RlbW8iLCJpYXQiOjE0Nzc0ODAxOTcsImF1ZCI6Imh0dHBzOi8vbWV0cmljcy1hcGkuaW9waXBlLmNvbS9ldmVudC8ifQ.rqy-hDI5x_nSJaQiVUviX5YH6OhzR7HMEQG79d_OuRw" })
 exports.handle = iopipe(
   function (event, context) {
-    var duration = 1001;
+    var duration = Math.floor(Math.random()*(2001-900+1)+900);
     setTimeout(function(){
       context.succeed('Succeeded with a set duration of ' + duration);
     }, duration);
