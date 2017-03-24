294558d9eb1bdd23f880299ecf0ebba9e6abb070
Pam Selle <pamela.selle@gmail.com>
2017-03-24 12:48:45 -0400

    Add Python funcs

---
+++ python/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+from iopipe.iopipe import IOpipe
+import time
+iopipe = IOpipe("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImF1dGgwfDU4MTA4ZWU4MDA0NDE0YWU2NzkxMzMyZCIsInVzZXJuYW1lIjoiaW9waXBlX2RlbW8iLCJpYXQiOjE0Nzc0ODAxOTcsImF1ZCI6Imh0dHBzOi8vbWV0cmljcy1hcGkuaW9waXBlLmNvbS9ldmVudC8ifQ.rqy-hDI5x_nSJaQiVUviX5YH6OhzR7HMEQG79d_OuRw")
+
+
+@iopipe.decorator
+def simpleSuccess(event, context):
+    iopipe.log("time", time.time())
+
+
+@iopipe.decorator
+def longRunning(event, context):
+    time.sleep(5)
+
+
+@iopipe.decorator
+def forcedFailure(event, context):
+    iopipe.log("time", time.time())
+    raise ValueError("this is a raised ValueError")
