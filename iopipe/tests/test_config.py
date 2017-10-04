75f2ba344910cf84e4d655fcd58fc6504c966225
Mislav Cimperšak <mislav.cimpersak@styria.hr>
2017-10-04 21:47:17 +0200

    additional tests for enabling/disabling iopipe

--- iopipe/tests/test_config.py
+++ iopipe/tests/test_config.py
@@ -8,15 +8,29 @@ def test_set_config__iopipe_enabled__default():
     assert config['enabled'] is True
 
 
-def test_set_config__iopipe_enabled__disabled():
+def test_set_config__iopipe_enabled__title_case_true():
+    os.environ['IOPIPE_ENABLED'] = 'True'
+
+    config = set_config()
+    assert config['enabled'] is True
+
+
+def test_set_config__iopipe_enabled__small_caps_true():
+    os.environ['IOPIPE_ENABLED'] = 'true'
+
+    config = set_config()
+    assert config['enabled'] is True
+
+
+def test_set_config__iopipe_enabled__title_case_false():
     os.environ['IOPIPE_ENABLED'] = 'False'
 
     config = set_config()
     assert config['enabled'] is False
 
 
-def test_set_config__iopipe_enabled__enabled():
-    os.environ['IOPIPE_ENABLED'] = 'True'
+def test_set_config__iopipe_enabled__small_caps_false():
+    os.environ['IOPIPE_ENABLED'] = 'false'
 
     config = set_config()
-    assert config['enabled'] is True
+    assert config['enabled'] is False
