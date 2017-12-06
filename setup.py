1e125e44a98a94ccc0dd817e7da5aad6f159095a
Michael Lavers <kolanos@gmail.com>
2017-12-06 08:54:05 -0800

    Simplifying dependencies
    * Removing `monotonic` and `requests` from `install_requires`
    * Adding `request` to `extras_Require`
    * Bundling `monotonic` with iopipe

--- setup.py
+++ setup.py
@@ -1,26 +1,16 @@
-#!/usr/bin/env python
-from pip.req import parse_requirements
-from pip.download import PipSession
 from setuptools import setup
 
-
-install_reqs = parse_requirements('./requirements.txt', session=PipSession())
-reqs = [str(ir.req) for ir in install_reqs]
-
-
-setup(name='iopipe',
-      version='0.9.1',
-      description='IOpipe agent for serverless Application Performance Monitoring',
-      author='IOpipe',
-      author_email='support@iopipe.com',
-      url='https://github.com/iopipe/iopipe-python',
-      packages=['iopipe'],
-      install_requires=reqs,
-      extras_require={
-          'dev': [
-              'flake8'
-          ]
-      },
-      setup_requires=['flake8', 'pytest-runner'],
-      tests_require=['mock', 'pytest', 'requests']
-      )
+setup(
+    name='iopipe',
+    version='0.9.1',
+    description='IOpipe agent for serverless Application Performance Monitoring',
+    author='IOpipe',
+    author_email='support@iopipe.com',
+    url='https://github.com/iopipe/iopipe-python',
+    packages=['iopipe'],
+    extras_require={
+        'dev': ['flake8', 'requests'],
+    },
+    setup_requires=['pytest-runner'],
+    tests_require=['mock', 'pytest', 'requests'],
+    zip_safe=True)
