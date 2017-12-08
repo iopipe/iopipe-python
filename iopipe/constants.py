eaeeba634a7371ba604b71981676f554a44c2fe3
Michael Lavers <kolanos@gmail.com>
2017-12-08 05:30:15 -0800

    Remove pkg_resources import (#123)
    Apparently some Python Lambda AMIs don't include `pkg_resources` which is
    supposed to be a part of the Python stdlib.

--- iopipe/constants.py
+++ iopipe/constants.py
@@ -1,4 +1,3 @@
-import pkg_resources
 import time
 import uuid
 
@@ -6,4 +5,4 @@ COLDSTART = True
 MODULE_LOAD_TIME = time.time() * 1000
 PROCESS_ID = str(uuid.uuid4())
 TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
-VERSION = pkg_resources.get_distribution('iopipe').version
+VERSION = '0.9.2'
