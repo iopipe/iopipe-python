d9e2af5f66e3b0c19d0e6e997e323da0dc04ab90
Michael Lavers <kolanos@gmail.com>
2017-12-22 08:17:09 -0800

    Adding marker tests, minor measure behavior change (#131)
    This adds tests for the `Marker` helper that gets attached to a context at
    invocation-time. It also makes a minor alteration to the `measure()` method
    behavior. The `start` and `end` arguments are now optional. Now
    `measure('foobar')` is equivalent to `measure('foobar', 'foobar', 'foobar')`
    and `measure('foobar', 'barbaz')` is equivalent to `measure('foobar',
    'barbaz', 'barbaz')`.

---
+++ tests/contrib/trace/test_marker.py
@@ -0,0 +1,33 @@
+def test_marker__measure_no_start_or_end(marker):
+    marker.start('foobar')
+    marker.end('foobar')
+    marker.measure('foobar')
+
+    assert len(marker.timeline.get_entries()) == 3
+    assert any([e.name == 'measure:foobar' for e in marker.timeline.get_entries()])
+
+
+def test_marker__measure_start_only(marker):
+    marker.start('foobar')
+    marker.end('foobar')
+    marker.end('barbaz')
+    marker.measure('barbaz', 'foobar')
+
+    assert len(marker.timeline.get_entries()) == 4
+    assert any([e.name == 'measure:barbaz' for e in marker.timeline.get_entries()])
+
+
+def test_marker__measure_different_start_and_end(marker):
+    marker.start('foobar')
+    marker.end('barbaz')
+    marker.measure('barbaz', 'foobar', 'barbaz')
+
+    assert len(marker.timeline.get_entries()) == 3
+    assert any([e.name == 'measure:barbaz' for e in marker.timeline.get_entries()])
+
+
+def test_marker__measure_no_marks(marker):
+    marker.measure('foobar')
+
+    assert len(marker.timeline.get_entries()) == 1
+    assert any([e.name == 'measure:foobar' for e in marker.timeline.get_entries()])
