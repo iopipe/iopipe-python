0b994d0bcc386d8b5f5aa7f1687640ace7be86d9
Michael Lavers <kolanos@gmail.com>
2018-01-02 11:00:58 -0800

    Making marker a context manager (#133)
    Updating the marker so that it can be used as a context manager, including nested context manager support.

--- iopipe/contrib/trace/marker.py
+++ iopipe/contrib/trace/marker.py
@@ -1,6 +1,21 @@
 class Marker(object):
     def __init__(self, timeline):
         self.timeline = timeline
+        self.contexts = []
+
+    def __call__(self, name):
+        self.contexts.append(name)
+        return self
+
+    def __enter__(self):
+        if self.contexts:
+            self.start(self.contexts[-1])
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if self.contexts:
+            self.end(self.contexts[-1])
+            self.contexts.pop()
 
     def start(self, name):
         self.timeline.mark('start:%s' % name)
