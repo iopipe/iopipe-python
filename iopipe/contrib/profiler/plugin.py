45087b469aad88e19c414b724b99ee878ea6f884
Michael Lavers <kolanos@gmail.com>
2018-02-16 15:09:07 -0800

    Upload the cProfile binary instead of pstats format
    Closes #156

--- iopipe/contrib/profiler/plugin.py
+++ iopipe/contrib/profiler/plugin.py
@@ -5,14 +5,7 @@ try:
     import cProfile as profile
 except ImportError:
     import profile
-import pstats
-try:
-    from cStringIO import StringIO
-except ImportError:
-    try:
-        from StringIO import StringIO
-    except ImportError:
-        from io import StringIO
+import tempfile
 
 from iopipe.plugins import Plugin
 
@@ -26,7 +19,7 @@ class ProfilerPlugin(Plugin):
     version = '0.1.0'
     homepage = 'https://github.com/iopipe/iopipe-python'
 
-    def __init__(self, enabled=False, sort='cumulative', restrictions=10):
+    def __init__(self, enabled=False):
         """
         Instantiates the profiler plugin
 
@@ -35,28 +28,8 @@ class ProfilerPlugin(Plugin):
                         the `IOPIPE_PROFILER_ENABLED` environment
                         variable.
         :type enabled: bool
-        :param sort: The column(s) in which to sort the stats. One or more
-                    columns can be set in the order in which they are to
-                    be sorted. Keep in mind that each column has an
-                    implicit sort order. Refer to pstats documentation for
-                    more information.
-        :type sort: str or list
-        :param restrictions: Any restrictions to be applied to the stats.
-                             Such as limiting results by count (int),
-                             percentage (float) or by regular expression (str). One
-                             or more restrictions can be applied via a list.
-                             See pstats documentation for more information.
-                    :type restrictions: int or float or str or list
         """
         self._enabled = enabled
-
-        self.sort = sort
-        if not isinstance(self.sort, (list, tuple)):
-            self.sort = [self.sort]
-
-        self.restrictions = restrictions or []
-        if not isinstance(self.restrictions, (list, tuple)):
-            self.restrictions = [self.restrictions]
 
     @property
     def enabled(self):
@@ -84,10 +57,7 @@ class ProfilerPlugin(Plugin):
 
     def post_report(self, report):
         if self.profile is not None:
-            self.stream = StringIO()
-            self.stats = pstats.Stats(self.profile, stream=self.stream)
-            self.stats.sort_stats(*self.sort)
-            self.stats.print_stats(*self.restrictions)
-
-            signed_request = get_signed_request(report)
-            upload_profiler_report(signed_request['signedRequest'], self.stream)
+            with tempfile.NamedTemporaryFile() as stats_file:
+                self.profile.dump_stats(stats_file.name)
+                signed_request = get_signed_request(report)
+                upload_profiler_report(signed_request['signedRequest'], stats_file.file)
