600165425f6a3fc28bf186df6644bdae7e47467c
Michael Lavers <kolanos@gmail.com>
2018-02-18 13:02:14 -0800

    Switching from jsonpath/ply to jmespath
    Still may need to be its own package

--- iopipe/contrib/eventinfo/util.py
+++ iopipe/contrib/eventinfo/util.py
@@ -1,31 +1,30 @@
-from .jsonpath import parser
+from . import jmespath
 
 # Caches previously seen parsers
-PARSERS = {}
+parsers = {}
+
+
+def _format_path(path):
+    return path.replace('["', '."').replace('.."', '."').replace('"]', '"')
+
+
+def _get_parser(path):
+    path = _format_path(path)
+    parser = parsers.get(path)
+    if parser is None:
+        parser = jmespath.compile(path)
+        parsers[path] = parser
+    return parser
 
 
 def get_value(obj, path):
-    return_length = False
     if path.endswith('.length'):
-        return_length = True
-        path = path.replace('.length', '')
-
-    parse = PARSERS.get(path)
-    if parse is None:
-        parse = parser.parse(path)
-        PARSERS[path] = parse
-
-    result = parse.find(obj)
-    if return_length:
-        return len(result)
-    return result[0].value if result else None
+        path = path.replace('.length', '|length(@)')
+    parser = _get_parser(path)
+    return parser.search(obj)
 
 
 def has_key(obj, path):
-    parse = PARSERS.get(path)
-    if parse is None:
-        parse = parser.parse(path)
-        PARSERS[path] = parse
-
-    result = parse.find(obj)
-    return len(result) > 0
+    parser = _get_parser(path)
+    result = parser.search(obj)
+    return result is not None
