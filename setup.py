40f0255f223240ffd83ecb2aaba9ece6ff2f602c
Michael Lavers <kolanos@gmail.com>
2018-02-22 13:43:46 -0800

    Update setup classifiers

--- setup.py
+++ setup.py
@@ -13,4 +13,17 @@ setup(
     },
     setup_requires=['pytest-runner'],
     tests_require=['jmespath>=0.7.1,<1.0.0', 'mock', 'pytest', 'pytest-benchmark', 'requests'],
-    zip_safe=True)
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ])
