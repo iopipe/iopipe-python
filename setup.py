bcac7da870033de916b55e7b0951e8a3cab08ac5
Michael Lavers <kolanos@gmail.com>
2018-02-26 12:58:59 -0800

    Bump to v1.2.0 (#165)
    * Bump to v1.2.0
    
    * Adds event info plugin in `iopipe.contrib.eventinfo` #160
    * Updates profiler plugin to provide the cProfile binary #158
    
    * Update setup classifiers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='iopipe',
-    version='1.1.0',
+    version='1.2.0',
     description='IOpipe agent for serverless Application Performance Monitoring',
     author='IOpipe',
     author_email='support@iopipe.com',
@@ -13,4 +13,17 @@ setup(
     },
     setup_requires=['pytest-runner'],
     tests_require=['jmespath>=0.7.1,<1.0.0', 'mock', 'pytest', 'pytest-benchmark', 'requests'],
-    zip_safe=True)
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ])
