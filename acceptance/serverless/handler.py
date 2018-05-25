71a8da611f8537bece6eb0bfc4d2f321c1cee5c9
Michael Lavers <kolanos@gmail.com>
2018-05-24 20:38:55 -0700

    A better sys.exit test
    Signed-off-by: Michael Lavers <kolanos@gmail.com>

--- acceptance/serverless/handler.py
+++ acceptance/serverless/handler.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     from urllib2 import urlopen
 
-from iopipe import constants, IOpipe, IOpipeCore
+from iopipe import IOpipe, IOpipeCore
 from iopipe.contrib.eventinfo import EventInfoPlugin
 from iopipe.contrib.logger import LoggerPlugin
 from iopipe.contrib.profiler import ProfilerPlugin
@@ -68,10 +68,14 @@ def caught_error(event, context):
         context.iopipe.error(e)
 
 
-@iopipe
 def coldstart(event, context):
-    if not constants.COLDSTART:
-        sys.exit(1)
+    @iopipe
+    def handler(event, context):
+        pass
+
+    handler(event, context)
+
+    sys.exit(1)
 
 
 @iopipe
