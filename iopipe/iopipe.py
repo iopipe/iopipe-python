af0553047a3d94d0f68da5a5660c100ea4bf3b4a
Michael Lavers <kolanos@gmail.com>
2018-07-12 16:25:54 -0700

    Remove logger plugin from kitchen sink, bump version
    Closes #246
    
    Signed-off-by: Michael Lavers <kolanos@gmail.com>

--- iopipe/iopipe.py
+++ iopipe/iopipe.py
@@ -1,7 +1,6 @@
 from .agent import IOpipeCore
 
 from .contrib.eventinfo import EventInfoPlugin
-from .contrib.logger import LoggerPlugin
 from .contrib.profiler import ProfilerPlugin
 from .contrib.trace import TracePlugin
 
@@ -13,7 +12,7 @@ class IOpipe(IOpipeCore):
 
     def __init__(self, *args, **kwargs):
         configured_plugins = kwargs.pop("plugins", None)
-        plugins = [EventInfoPlugin(), LoggerPlugin(), ProfilerPlugin(), TracePlugin()]
+        plugins = [EventInfoPlugin(), ProfilerPlugin(), TracePlugin()]
         if configured_plugins is not None and isinstance(plugins, list):
             plugins = plugins + configured_plugins
         kwargs["plugins"] = plugins
--- tests/test_iopipe.py
+++ tests/test_iopipe.py
@@ -2,21 +2,19 @@ def test_default_iopipe(default_iopipe):
     """Asserts that the default agent comes preloaded"""
     iopipe = default_iopipe
 
-    assert len(iopipe.plugins) == 4
+    assert len(iopipe.plugins) == 3
     assert iopipe.plugins[0].name == "event-info"
-    assert iopipe.plugins[1].name == "logger"
-    assert iopipe.plugins[2].name == "profiler"
-    assert iopipe.plugins[3].name == "trace"
+    assert iopipe.plugins[1].name == "profiler"
+    assert iopipe.plugins[2].name == "trace"
 
 
 def test_default_iopipe_override(default_iopipe_override):
     """Asserts the default agent can be overridden"""
     iopipe = default_iopipe_override
 
-    assert len(iopipe.plugins) == 4
+    assert len(iopipe.plugins) == 3
     assert iopipe.plugins[0].name == "event-info"
-    assert iopipe.plugins[1].name == "logger"
-    assert iopipe.plugins[2].name == "profiler"
-    assert iopipe.plugins[3].name == "trace"
+    assert iopipe.plugins[1].name == "profiler"
+    assert iopipe.plugins[2].name == "trace"
 
-    assert iopipe.plugins[3].auto_measure is False
+    assert iopipe.plugins[2].auto_measure is False
