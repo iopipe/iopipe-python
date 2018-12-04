969b957ee1e27f6492951d883e115376d680aef6
Michael Lavers <kolanos@gmail.com>
2018-12-04 14:09:24 -0800

    Circle doesn't support volumes, can't use docker

--- publish-layers.sh
+++ publish-layers.sh
@@ -4,7 +4,7 @@ PY27_DIST=dist/python27.zip
 PY3X_DIST=dist/python3x.zip
 
 REGIONS=(
-  #ap-northeast-1
+  ap-northeast-1
   ap-northeast-2
   ap-south-1
   ap-southeast-1
@@ -24,80 +24,91 @@ REGIONS=(
 rm -rf dist python
 mkdir -p dist
 
-echo "Building iopipe layer for python2.7"
-docker run --rm -v "$PWD":/var/task lambci/lambda:build-python2.7 pip install -qU iopipe -t python/lib/python2.7/site-packages \
-    && find python -name '*.pyc' -exec rm -f {} + \
-    && zip -rq $PY27_DIST python \
-    && rm -rf python \
-    && echo "Build complete: ${PY27_DIST}" \
-    || echo "Build failure: ${PY27_DIST}"
+function usage {
+    echo "./publish-layers.sh [python2.7|python3.x]"
+}
 
-echo "Building iopipe Layer for python3.x"
-docker run --rm -v "$PWD":/var/task lambci/lambda:build-python3.6 pip install -qU iopipe -t python \
-    && find python -name '__pycache__' -exec rm -fr {} + \
-    && zip -rq $PY3X_DIST python \
-    && rm -rf python \
-    && echo "Build complete: ${PY3X_DIST}" \
-    || echo "Build failure: ${PY3X_DIST}"
+case "$1" in
+    "python2.7")
+        echo "Building iopipe layer for python2.7"
+        pip install -qU iopipe -t python/lib/python2.7/site-packages
+        find python -name '*.pyc' -exec rm -f {} +
+        zip -rq $PY27_DIST python
+        rm -rf python
+        echo "Build complete: ${PY27_DIST}"
 
-PY27_MD5SUM=$(md5 -q $PY27_DIST)
-PY27_S3KEY="iopipe-python2.7/${PY27_MD5SUM}"
+        py27_md5sum=$(md5 -q $PY27_DIST)
+        py27_s3key="iopipe-python2.7/${py27_md5sum}.zip"
 
-PY3X_MD5SUM=$(md5 -q $PY3X_DIST)
-PY3X_S3KEY="iopipe-python3.x/${PY3X_MD5SUM}"
+        for region in "${REGIONS[@]}"; do
+            bucket_name="iopipe-layers-${region}"
 
-for region in "${REGIONS[@]}"; do
-	bucket_name="iopipe-layers-${region}"
+            echo "Uploading ${PY27_DIST} to s3://${bucket_name}/${py27_s3key}"
+            aws --region $region s3 cp $PY27_DIST "s3://${bucket_name}/${py27_s3key}"
 
-	echo "Uploading ${PY27_DIST} to s3://${bucket_name}/${PY27_S3KEY}.zip"
-	aws --region $region s3 cp $PY27_DIST "s3://${bucket_name}/${PY27_S3KEY}.zip"
+            echo "Publishing python2.7 layer to ${region}"
+            py27_version=$(aws lambda publish-layer-version \
+                --layer-name IOpipePython27 \
+                --content "S3Bucket=${bucket_name},S3Key=${py27_s3key}" \
+                --description "IOpipe Layer for Python 2.7" \
+                --compatible-runtimes python2.7 \
+                --license-info "Apache 2.0" \
+                --region $region \
+                --output text \
+                --query Version)
+            echo "Published python2.7 layer version ${py27_version} to ${region}"
 
-	echo "Uploading ${PY3X_DIST} to s3://${bucket_name}/${PY3X_S3KEY}.zip"
-	aws --region $region s3 cp $PY3X_DIST "s3://${bucket_name}/${PY3X_S3KEY}.zip"
+            echo "Setting public permissions for python2.7 layer version ${py27_version} in ${region}"
+            aws lambda add-layer-version-permission \
+              --layer-name IOpipePython27 \
+              --version-number $py27_version \
+              --statement-id public \
+              --action lambda:GetLayerVersion \
+              --principal "*"
+            echo "Public permissions set for python2.7 layer version ${py27_version} in region ${region}"
+        done
+        ;;
+    "python3.x")
+        echo "Building iopipe Layer for python3.x"
+        pip install -qU iopipe -t python
+        find python -name '__pycache__' -exec rm -fr {} +
+        zip -rq $PY3X_DIST python
+        rm -rf python
+        echo "Build complete: ${PY3X_DIST}" \
 
-	echo "Publishing python2.7 layer to ${region}"
-	py27_version=$(aws lambda publish-layer-version \
-		--layer-name IOpipePython27 \
-		--content "S3Bucket=${bucket_name},S3Key=${PY27_S3KEY}.zip" \
-		--description "IOpipe Layer for Python 2.7" \
-		--compatible-runtimes python2.7 \
-		--license-info "Apache 2.0" \
-		--region $region \
-		--output text \
-		--query Version)
-	echo "Published python2.7 layer version ${py27_version} to ${region}"
+        py3x_md5sum=$(md5 -q $PY3X_DIST)
+        py3x_s3key="iopipe-python3.x/${py3x_md5sum}.zip"
 
-	echo "Setting public permissions for python2.7 layer version ${py27_version} in ${region}"
-	aws lambda add-layer-version-permission \
-      --layer-name IOpipePython27 \
-      --version-number $py27_version \
-      --statement-id public \
-      --action lambda:GetLayerVersion \
-      --principal "*" \
-      --region $region \
-      > /dev/null
-	echo "Public permissions set for python2.7 layer version ${py27_version} in region ${region}"
+        for region in "${REGIONS[@]}"; do
+            bucket_name="iopipe-layers-${region}"
 
-	echo "Publishing python3.x layer to ${region}"
-	py3x_version=$(aws lambda publish-layer-version \
-		--layer-name IOpipePython \
-		--content "S3Bucket=${bucket_name},S3Key=${PY3X_S3KEY}.zip" \
-		--description "IOpipe Layer for Python 3.6+" \
-		--compatible-runtimes python3.6 python3.7 \
-		--license-info "Apache 2.0" \
-		--region $region \
-		--output text \
-		--query Version)
-	echo "published python3.x layer version ${py3x_version} to ${region}"
+            echo "Uploading ${PY3X_DIST} to s3://${bucket_name}/${py3x_s3key}"
+            aws --region $region s3 cp $PY3X_DIST "s3://${bucket_name}/{$py3x_s3key}"
 
-	echo "Setting public permissions for python3.x layer version ${py3x_version} in ${region}"
-	aws lambda add-layer-version-permission \
-      --layer-name IOpipePython \
-      --version-number $py3x_version \
-      --statement-id public \
-      --action lambda:GetLayerVersion \
-      --principal "*" \
-      --region $region \
-      > /dev/null
-	echo "Public permissions set for python3.x Layer version ${py3x_version} in region ${region}"
-done
+            echo "Publishing python3.x layer to ${region}"
+            py3x_version=$(aws lambda publish-layer-version \
+                --layer-name IOpipePython \
+                --content "S3Bucket=${bucket_name},S3Key=${py3x_s3key}" \
+                --description "IOpipe Layer for Python 3.6+" \
+                --compatible-runtimes python3.6 python3.7 \
+                --license-info "Apache 2.0" \
+                --region $region \
+                --output text \
+                --query Version)
+            echo "published python3.x layer version ${py3x_version} to ${region}"
+
+            echo "Setting public permissions for python3.x layer version ${py3x_version} in ${region}"
+            aws lambda add-layer-version-permission \
+              --layer-name IOpipePython \
+              --version-number $py3x_version \
+              --statement-id public \
+              --action lambda:GetLayerVersion \
+              --principal "*" \
+              --region $region
+            echo "Public permissions set for python3.x Layer version ${py3x_version} in region ${region}"
+        done
+        ;;
+    *)
+        usage
+        ;;
+esac
