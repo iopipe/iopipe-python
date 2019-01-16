2ecf8483e269c2e6d34931c64f6ec46e7c96ab67
Michael Lavers <kolanos@gmail.com>
2019-01-16 07:25:51 -0800

    Pass StreamToLogger methods to sys.__stdout__
    In Python 3.7, `print()` calls `flush()` on stdout. Make sure this and all other method calls continue to work.
    
    Closes #299

--- iopipe/contrib/logger/stream.py
+++ iopipe/contrib/logger/stream.py
@@ -1,7 +1,14 @@
+import sys
+
+
 class StreamToLogger(object):
     def __init__(self, logger):
         self.logger = logger
 
+    def __getattr__(self, name):
+        return getattr(sys.__stdout__, name)
+
     def write(self, buf):
         for line in buf.rstrip().splitlines():
             self.logger.info(line.rstrip())
+        sys.__stdout__.write(buf)
