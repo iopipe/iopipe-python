d4194a59dc1fdcc17fdc61545bab9245bd9cdfc5
Michael Lavers <kolanos@gmail.com>
2019-01-16 10:33:17 -0800

    Disable stdout redirection for python3.7
    In the Python 3.7 runtime, lambda configures a logging handler that writes to stdout resulting in a recursion error. Disable stdout redirection in python3.7 for now.
    
    Related to #299

--- iopipe/compat.py
+++ iopipe/compat.py
@@ -2,6 +2,9 @@ import io
 import sys
 
 PY3 = sys.version_info[0] == 3
+PY3 = sys.version_info[0] == 3
+PY37 = sys.version_info[0] == 3 and sys.version_info[1] == 7
+
 
 if PY3:  # pragma: no cover
     from urllib.parse import urlparse
