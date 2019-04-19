bdd492406392102a1b43a65681d0d1ebb7c903b8
Michael Lavers <kolanos@gmail.com>
2019-04-19 10:29:49 -0700

    Add AWS Regions

--- iopipe/collector.py
+++ iopipe/collector.py
@@ -9,8 +9,14 @@ except ImportError:
 
 SUPPORTED_REGIONS = [
     "ap-northeast-1",
+    "ap-northeast-2",
+    "ap-south-1",
+    "ap-southeast-1",
     "ap-southeast-2",
+    "ca-central-1",
+    "eu-central-1",
     "eu-west-1",
+    "eu-west-2",
     "us-east-2",
     "us-west-1",
     "us-west-2",
