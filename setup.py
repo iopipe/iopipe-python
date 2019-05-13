89ad47926a2fbf64323b5dc71b2179507f1749af
Michael Lavers <kolanos@gmail.com>
2019-05-13 12:56:34 -0700

    Bump to v1.7.18

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ coverage_requires = tests_require + ["coverage==5.0a2", "pytest-cov==2.6.1"]
 
 setup(
     name="iopipe",
-    version="1.7.17",
+    version="1.7.18",
     description="IOpipe agent for serverless Application Performance Monitoring",
     long_description=README,
     author="IOpipe",
