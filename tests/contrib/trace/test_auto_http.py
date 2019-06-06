43828d587417f6376993dc4851fe45c54a6a23ee
Michael Lavers <kolanos@gmail.com>
2019-06-06 08:45:50 -0700

    Add monkey patching for botocore.httpsession (#321)
    * Add monkey patching for botocore.httpsession
    
    Closes #319
    
    * Rmove AWSResponse, don't need it
    
    * Add monkey patching test
    
    * Add mock filter to test
    
    * Make monkey patching python2.7 compatible

---
+++ tests/contrib/trace/test_auto_http.py
@@ -0,0 +1,26 @@
+from botocore.httpsession import URLLib3Session as BotocoreSession
+from botocore.vendored.requests.sessions import Session as BotocoreVendoredSession
+from requests.sessions import Session as RequestsSession
+
+from iopipe.contrib.trace.auto_http import patch_requests, restore_requests
+
+
+def test_monkey_patching(mock_context):
+    assert not hasattr(RequestsSession, "__monkey_patched")
+    assert not hasattr(BotocoreSession, "__monkey_patched")
+    assert not hasattr(BotocoreVendoredSession, "__monkey_patched")
+
+    def mock_filter(http_response):
+        return http_response
+
+    patch_requests(mock_context, mock_filter)
+
+    assert hasattr(RequestsSession, "__monkey_patched")
+    assert hasattr(BotocoreSession, "__monkey_patched")
+    assert hasattr(BotocoreVendoredSession, "__monkey_patched")
+
+    restore_requests()
+
+    assert not hasattr(RequestsSession, "__monkey_patched")
+    assert not hasattr(BotocoreSession, "__monkey_patched")
+    assert not hasattr(BotocoreVendoredSession, "__monkey_patched")
