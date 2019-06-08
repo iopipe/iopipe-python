a3cc53e2d45616ba067bbfdc9d7510f08456ffa1
Michael Lavers <kolanos@gmail.com>
2019-06-07 18:40:01 -0700

    Limit HTTP headers collected, allow overide
    Closes #274 #305

--- tests/contrib/trace/test_auto_http.py
+++ tests/contrib/trace/test_auto_http.py
@@ -2,7 +2,7 @@ from botocore.httpsession import URLLib3Session as BotocoreSession
 from botocore.vendored.requests.sessions import Session as BotocoreVendoredSession
 from requests.sessions import Session as RequestsSession
 
-from iopipe.contrib.trace.auto_http import patch_requests, restore_requests
+from iopipe.contrib.trace.auto_http import patch_http_requests, restore_http_requests
 
 
 def test_monkey_patching(mock_context):
@@ -13,13 +13,15 @@ def test_monkey_patching(mock_context):
     def mock_filter(request, response):
         return request, response
 
-    patch_requests(mock_context, mock_filter)
+    http_headers = ["Cache-Control"]
+
+    patch_http_requests(mock_context, mock_filter, http_headers)
 
     assert hasattr(RequestsSession, "__monkey_patched")
     assert hasattr(BotocoreSession, "__monkey_patched")
     assert hasattr(BotocoreVendoredSession, "__monkey_patched")
 
-    restore_requests()
+    restore_http_requests()
 
     assert not hasattr(RequestsSession, "__monkey_patched")
     assert not hasattr(BotocoreSession, "__monkey_patched")
