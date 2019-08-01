b79a8013489ec3031eec093ba33d6bf805c8ab08
Michael Lavers <kolanos@gmail.com>
2019-08-01 15:55:45 -0700

    Testing wrapt

--- tests/contrib/trace/test_auto_http.py
+++ tests/contrib/trace/test_auto_http.py
@@ -1,5 +1,3 @@
-import mock
-
 from botocore.httpsession import URLLib3Session as BotocoreSession
 from botocore.vendored.requests.sessions import Session as BotocoreVendoredSession
 from requests.sessions import Session as RequestsSession
@@ -7,18 +5,10 @@ from requests.sessions import Session as RequestsSession
 from iopipe.contrib.trace.auto_http import patch_http_requests, restore_http_requests
 
 
-@mock.patch("iopipe.contrib.trace.auto_http.original_requests_session_send")
-@mock.patch("iopipe.contrib.trace.auto_http.original_botocore_session_send")
-@mock.patch("iopipe.contrib.trace.auto_http.original_botocore_vendored_session_send")
-def test_monkey_patching(
-    mock_botocore_vendored_session_send,
-    mock_botocore_session_send,
-    mock_requests_session_send,
-    mock_context_wrapper,
-):
-    assert not hasattr(RequestsSession, "__monkey_patched")
-    assert not hasattr(BotocoreSession, "__monkey_patched")
-    assert not hasattr(BotocoreVendoredSession, "__monkey_patched")
+def test_patch_http_requests(mock_context_wrapper,):
+    assert not hasattr(BotocoreSession.send, "__wrapped__")
+    assert not hasattr(BotocoreVendoredSession.send, "__wrapped__")
+    assert not hasattr(RequestsSession.send, "__wrapped__")
 
     def mock_filter(request, response):
         return request, response
@@ -27,64 +17,29 @@ def test_monkey_patching(
 
     patch_http_requests(mock_context_wrapper, mock_filter, http_headers)
 
-    assert hasattr(RequestsSession, "__monkey_patched")
-    assert hasattr(BotocoreSession, "__monkey_patched")
-    assert hasattr(BotocoreVendoredSession, "__monkey_patched")
-
-    requests_session = RequestsSession()
-    requests_session.send()
-    assert mock_requests_session_send.called
-
-    botocore_session = BotocoreSession()
-    botocore_session.send()
-    assert mock_botocore_session_send.called
-
-    botocore_vendored_session = BotocoreVendoredSession()
-    botocore_vendored_session.send()
-    assert mock_botocore_vendored_session_send.called
+    assert hasattr(BotocoreSession.send, "__wrapped__")
+    assert hasattr(BotocoreVendoredSession.send, "__wrapped__")
+    assert hasattr(RequestsSession.send, "__wrapped__")
 
     restore_http_requests()
 
-    assert not hasattr(RequestsSession, "__monkey_patched")
-    assert not hasattr(BotocoreSession, "__monkey_patched")
-    assert not hasattr(BotocoreVendoredSession, "__monkey_patched")
+    assert not hasattr(BotocoreSession.send, "__wrapped__")
+    assert not hasattr(BotocoreVendoredSession.send, "__wrapped__")
+    assert not hasattr(RequestsSession.send, "__wrapped__")
 
 
-@mock.patch("iopipe.contrib.trace.auto_http.original_requests_session_send")
-@mock.patch("iopipe.contrib.trace.auto_http.original_botocore_session_send")
-@mock.patch("iopipe.contrib.trace.auto_http.original_botocore_vendored_session_send")
-def test_monkey_patching_no_iopipe(
-    mock_botocore_vendored_session_send,
-    mock_botocore_session_send,
-    mock_requests_session_send,
-    mock_context,
-):
-    assert not hasattr(RequestsSession, "__monkey_patched")
-    assert not hasattr(BotocoreSession, "__monkey_patched")
-    assert not hasattr(BotocoreVendoredSession, "__monkey_patched")
+def test_patch_http_requests_no_iopipe(mock_context,):
+    """Asserts that monkey patching does not occur if IOpipe not present"""
+    restore_http_requests()
+
+    assert not hasattr(BotocoreSession.send, "__wrapped__")
+    assert not hasattr(BotocoreVendoredSession.send, "__wrapped__")
+    assert not hasattr(RequestsSession.send, "__wrapped__")
 
     delattr(mock_context, "iopipe")
 
     patch_http_requests(mock_context, None, None)
 
-    assert hasattr(RequestsSession, "__monkey_patched")
-    assert hasattr(BotocoreSession, "__monkey_patched")
-    assert hasattr(BotocoreVendoredSession, "__monkey_patched")
-
-    requests_session = RequestsSession()
-    requests_session.send()
-    assert mock_requests_session_send.called
-
-    botocore_session = BotocoreSession()
-    botocore_session.send()
-    assert mock_botocore_session_send.called
-
-    botocore_vendored_session = BotocoreVendoredSession()
-    botocore_vendored_session.send()
-    assert mock_botocore_vendored_session_send.called
-
-    restore_http_requests()
-
-    assert not hasattr(RequestsSession, "__monkey_patched")
-    assert not hasattr(BotocoreSession, "__monkey_patched")
-    assert not hasattr(BotocoreVendoredSession, "__monkey_patched")
+    assert not hasattr(BotocoreSession.send, "__wrapped__")
+    assert not hasattr(BotocoreVendoredSession.send, "__wrapped__")
+    assert not hasattr(RequestsSession.send, "__wrapped__")
