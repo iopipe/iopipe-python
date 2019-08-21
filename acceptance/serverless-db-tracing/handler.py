2deedb7cf1c985f3b21cd5e3c72583f1c7ede0bc
Michael Lavers <kolanos@gmail.com>
2019-08-21 13:52:16 -0700

    Add mysql database tracing acceptance test

--- acceptance/serverless-db-tracing/handler.py
+++ acceptance/serverless-db-tracing/handler.py
@@ -1,5 +1,6 @@
 import os
 
+import MySQLdb
 import psycopg2
 
 from iopipe import IOpipeCore
@@ -10,7 +11,28 @@ iopipe = IOpipeCore(debug=True, plugins=[trace_plugin])
 
 
 @iopipe
-def postgres(event, context):
+def _mysqldb(event, context):
+    conn = MySQLdb.connect(
+        dbname=os.environ["DB_NAME"],
+        host=os.environ["MYSQL_HOST"],
+        passwd=os.environ["DB_PASSWORD"],
+        port=os.environ["MYSQL_PORT"],
+        username=os.environ["DB_USERNAME"],
+    )
+    cur = conn.cursor()
+
+    cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+    cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abcdef"))
+
+    cur.execute("SELECT * FROM test")
+    cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+
+@iopipe
+def _psycopg2(event, context):
     conn = psycopg2.connect(
         "postgres://%s:%s@%s:%s/%s"
         % (
