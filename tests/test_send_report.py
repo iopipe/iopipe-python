24d4491597b17fe563c3a800359b5cc49bb928b0
Vlad Emelianov <volshebnyi@gmail.com>
2019-10-29 23:13:07 +0100

    Fix unit tests for send_report to not depend on ENV

--- tests/test_send_report.py
+++ tests/test_send_report.py
@@ -4,6 +4,7 @@ from iopipe.config import set_config
 from iopipe.send_report import send_report
 
 
+@mock.patch("os.environ", {})
 @mock.patch("iopipe.send_report.session", autospec=True)
 def test_send_report(mock_session):
     """Assert that a POST request is made when a report is sent"""
@@ -17,6 +18,7 @@ def test_send_report(mock_session):
     )
 
 
+@mock.patch("os.environ", {})
 @mock.patch("iopipe.send_report.session", autospec=True)
 def test_send_report_network_timeout(mock_session):
     """Assert that the timeout is changed when network_timeout is set"""
